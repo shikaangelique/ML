ba90d306109920573c264e6fb486430794f78b00
Angelique Shika <shikaangelique@gmail.com>
2022-05-25 15:06:32 +0000

    Initial commit

---
+++ Sarcasm Detector/sarcasm_detector.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import BernoulliNB
+
+data = pd.read_json("sarcasm.json")
+data["is_sarcastic"] = data["is_sarcastic"].map({0: "Not Sarcasm", 1: "Sarcasm"})
+print(data.head())
+
+data = data[["headline", "is_sarcastic"]]
+x = np.array(data["headline"])
+y = np.array(data["is_sarcastic"])
+
+cv = CountVectorizer()
+X = cv.fit_transform(x) # Fit the Data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
